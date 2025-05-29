f8ea53972f48612372fc7fbb5c06a4932a06a9cb
aryanshs01 <aryanshsharma612@gmail.com>
2025-05-29 13:05:34 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 snowflake_conn_config = {
     "account": "xtnryzd-wra37379",          
     "user": "aryanshs01",                
-    "password": "aRYANSHSHARMA_8953",        
+    "password": "XXXXXXXXXXXXXXXX",        
     "warehouse": "COMPUTE_WH",              
     "database": "DEMO_DB",                
     "schema": "PUBLIC",                    
